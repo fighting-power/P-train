47741ea004dc0cdeb288a7dc8bd382a6ade80c1a
fighting-power <martinbeije@hotmail.com>
2023-02-03 12:01:30 +0100

    Aanpassing aan opdracht4 doorgevoerd

--- Huiswerk Opdracht 4.ps1
+++ Huiswerk Opdracht 4.ps1
@@ -5,14 +5,14 @@ $OU = $null
 $TestOU = $null
 $Password = $null
 
-"Geef de naam van de aan te maken gebruiker op alsjeblieft?"
+"Geef de Gebruikersnaam van de aan te maken gebruiker op alsjeblieft?"
 $Username = Read-Host
 
 "Geef de Voornaam van de aan te maken gebruiker op alsjeblieft?"
 $FirstName = Read-Host
 
 "Geef de Achternaam van de aan te maken gebruiker op alsjeblieft?"
-$FirstName = Read-Host
+$LastName = Read-Host
 
 "Geef het aan te maken wachtwoord voor de gebruiker op alsjeblieft"
 $Password = Read-Host -AsSecureString
@@ -20,20 +20,19 @@ $Password = Read-Host -AsSecureString
 "Geef de naam van de OU op waarin de gebruiker aangemaakt moet worden?"
 $OU = Read-Host
 
-$TestOU = Get-ADOrganizationalUnit -Identity "OU=$OU,DC=ADATUM,DC=COM" -ErrorAction 'SilentlyContinue'
+$TestOU = Get-ADOrganizationalUnit -Identity "OU=$OU,DC=ADATUM,DC=COM" -ErrorAction SilentlyContinue
 
 if ($null-eq $TestOU)
 {
 New-ADOrganizationalUnit -Name $OU
 'Nieuwe OU '+$OU +' is aangemaakt'
-New-ADUser -Name $Username -GivenName $FirstName -Surname $LastName -AccountPassword $Password -Enabled $true
+New-ADUser -Name $Username -GivenName $FirstName -Surname $LastName -AccountPassword $Password -Path "OU=$OU,DC=ADATUM,DC=COM" -Enabled $true
+Get-ADUser -Identity $Username
 }
 else
 {
 'OU bestaat Al.'
-'We maken geen nieuwe '+$OU +'aan'
+'We maken geen nieuwe '+$OU +' aan'
+New-ADUser -Name $Username -GivenName $FirstName -Surname $LastName -AccountPassword $Password -Path "OU=$OU,DC=ADATUM,DC=COM" -Enabled $true
+Get-ADUser -Identity $Username
 }
-
-
-
-#New-ADUser -Name $username -AccountPassword $Password -Enabled $true
